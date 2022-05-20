9050108847e71cebc54e5c6b7e74b946c9d6628b
NicolaVarriale94 <nicola.varriale.1994@gmail.com>
2022-05-20 10:13:01 +0200

    ricaricato db perchè ...
    ricaricato il db con le tabelle

--- Database/giuseppetiodio.sql
+++ Database/giuseppetiodio.sql
@@ -34,7 +34,11 @@ CREATE TABLE IF NOT EXISTS `anagraphic` (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella libreria.anagraphic: ~3 rows (circa)
+INSERT INTO `anagraphic` (`id`, `name`, `surname`, `mail`, `gender`, `street`, `cn`, `cap`, `town`, `phone`) VALUES
+	(5, 'Nicola', 'Varriale', 'nicola@esempio.it', 'M', 'via garibaldi', '13', '80100', 'Napoli', '3471567845'),
+	(6, 'Cristina', 'Campo', 'cristina.campo@esempio.it', 'F', 'via dei ciliegi', '12', '70100', 'Bari', '2356897845'),
+	(7, 'Giuseppe', 'Alfiero', 'giuseppe.alfiero@esempio.it', 'M', 'via dei monti', '22', '80100', 'Napoli', '1254874815');
 
 -- Dump della struttura di tabella libreria.books
 CREATE TABLE IF NOT EXISTS `books` (
@@ -51,7 +55,10 @@ CREATE TABLE IF NOT EXISTS `books` (
   CONSTRAINT `FK_books_categories` FOREIGN KEY (`id_category`) REFERENCES `categories` (`id_category`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella libreria.books: ~2 rows (circa)
+INSERT INTO `books` (`id_book`, `title`, `author`, `public_year`, `description`, `price`, `quantity`, `id_category`) VALUES
+	(1, 'La mappa dei desideri', 'Carrie Ryan , John Parke Davis', '2014-05-15', 'Chi possiede la Mappa dei Desideri può arrivare ovunque voglia. Per il momento però nessuno può servirsi dei suoi poteri, perché è stata divisa in brandelli: il primo, la Rosa dei Venti, appare all\'improvviso nel parcheggio di un supermercato insieme a un\'onda d\'acqua solcata da un vascello di corsari, sotto lo sguardo incredulo della dodicenne Marrill. Intenta a inseguire il suo gatto, Marrill si trova catapultata nella Corrente Pirata, un mondo popolato da foreste parlanti, farfalle di spuma di mare e crostacei con le piume. Soltanto la mappa può ricondurla nel suo mondo. E Marrill non è l\'unica a cercarla: Fin, cresciuto nella Corrente Pirata, a dodici anni è già il Maestro dei Ladri grazie a un raro potere: chiunque lo incontri ben presto si dimentica di lui. Guidato da una misteriosa profezia, Fin vuole la mappa per ritrovare la madre scomparsa. Quando i destini di Marrill e Fin incrociano la rotta di una ciurma di temibili pirati, la ricerca della mappa si trasforma in una questione di vita o di morte. Età di lettura: da 11 anni.', 30.5, 10, 1),
+	(2, 'Lavorare con intelligenza emotiva', 'Daniel Goleman, ', '2000-03-23', 'Saper gestire le proprie emozioni nelle varie situazioni a cui la vita ci mette di fronte, può essere una qualità determinante per raggiungere risultati importanti sia nel lavoro, che nella sfera personale.', 17.5, 15, 5);
 
 -- Dump della struttura di tabella libreria.categories
 CREATE TABLE IF NOT EXISTS `categories` (
@@ -61,7 +68,14 @@ CREATE TABLE IF NOT EXISTS `categories` (
   PRIMARY KEY (`id_category`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella libreria.categories: ~6 rows (circa)
+INSERT INTO `categories` (`id_category`, `name`, `description`) VALUES
+	(1, 'Avventura', 'Il romanzo di avventura è un genere letterario che nasce nel XVIII secolo e narra di viaggi in terre lontane e quindi celebra il coraggio e l\'ingegno umano.'),
+	(2, 'Storico', 'Un romanzo storico è un\'opera narrativa ambientata nel passato, con un\'accurata ricostruzione dell\'epoca attraverso atmosfere, costumi, usanze, condizioni sociali e mentalità dei personaggi principali.'),
+	(3, 'Fantasy', 'Il termine fantasy racchiude un insieme di elementi imprescindibili: soprannaturale: in questi romanzi è preponderante la tematica sovrannaturale, che consta di ambientazioni, fenomeni e creature che non appartengono alla nostra dimensione o comunque la distorcono o arricchiscono con particolari insoliti.'),
+	(4, 'Horror', 'Oggi esiste un preciso genere narrativo, il genere horror, basato proprio su storie che riescono a provocare nel lettore orrore e paura. Il racconto o il romanzo horror si chiama anche gotico, di fantasmi (o ghost story, all\'inglese), del terrore e così via.'),
+	(5, 'Saggistica', 'Per saggistica si intende l\'insieme dei testi che rientrano nel genere del saggio, esattamente come per narrativa intendiamo l\'insieme dei testi che rientrano nel genere del romanzo.'),
+	(6, 'Romantico', 'Il romanzo rosa (detto anche romance) è un genere letterario che narra di storie d\'amore e del loro intreccio che si dipanano in genere in avventure e intrighi e terminano sempre con un lieto fine.');
 
 -- Dump della struttura di tabella libreria.log_ordini
 CREATE TABLE IF NOT EXISTS `log_ordini` (
@@ -71,7 +85,11 @@ CREATE TABLE IF NOT EXISTS `log_ordini` (
   PRIMARY KEY (`id_log_ordini`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella libreria.log_ordini: ~3 rows (circa)
+INSERT INTO `log_ordini` (`id_log_ordini`, `id_ordine`, `descrizione_stato`) VALUES
+	(1, 1, 'L\'ordine è stato spedito all\'indirizzo.'),
+	(2, 1, 'In consegna'),
+	(3, 1, 'L\'ordine è stato rifiutato.');
 
 -- Dump della struttura di tabella libreria.order_list
 CREATE TABLE IF NOT EXISTS `order_list` (
@@ -86,7 +104,10 @@ CREATE TABLE IF NOT EXISTS `order_list` (
   CONSTRAINT `order_list_ibfk_2` FOREIGN KEY (`id_book`) REFERENCES `books` (`id_book`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella libreria.order_list: ~2 rows (circa)
+INSERT INTO `order_list` (`id_order`, `id_anagraphic`, `id_book`, `status`) VALUES
+	(2, 5, 1, 'SPEDITO'),
+	(3, 6, 2, 'CONSEGNATO');
 
 -- Dump della struttura di tabella libreria.privileges
 CREATE TABLE IF NOT EXISTS `privileges` (
@@ -98,7 +119,12 @@ CREATE TABLE IF NOT EXISTS `privileges` (
   PRIMARY KEY (`id_privileges`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella libreria.privileges: ~4 rows (circa)
+INSERT INTO `privileges` (`id_privileges`, `name`, `edit_book`, `edit_user`, `edit_admin`) VALUES
+	(1, 'Amministratore', 'Y', 'Y', 'Y'),
+	(2, 'Super Moderatore', 'Y', 'Y', 'N'),
+	(3, 'Moderatore', 'Y', 'N', 'N'),
+	(4, 'Utente', 'N', 'N', 'N');
 
 -- Dump della struttura di tabella libreria.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -114,7 +140,10 @@ CREATE TABLE IF NOT EXISTS `users` (
   CONSTRAINT `FK_users_privileges` FOREIGN KEY (`id_privileges`) REFERENCES `privileges` (`id_privileges`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella libreria.users: ~2 rows (circa)
+INSERT INTO `users` (`id`, `username`, `password`, `id_privileges`, `anagraphic_id`) VALUES
+	(6, 'utente', 'utente', 4, 5),
+	(7, 'admin', 'admin', 1, 5);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
